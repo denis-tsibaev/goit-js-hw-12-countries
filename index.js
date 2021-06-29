21c7b77d60d910efaaaf92a6feccd9274cd2cc99
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-29 23:31:43 +0700

    коммит

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+// fetch("https://restcountries.eu/rest/v2")
+//   .then((response) => {
+//     return response.json;
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     error;
+//   });
+
+const inputCountry = document.querySelector('.input');
+inputCountry.addEventListener('input', onSearch)
+
+function onSearch(event) {
+	const searchQuery = event.target.value.trim();
+}
+

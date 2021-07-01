4b65658d4e60581ab5d09ba49de771c6643785b0
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-07-01 10:44:17 +0700

    Немного уменшил код чтобы найти ошибку рендера

--- src/index.js
+++ src/index.js
@@ -6,41 +6,15 @@ import { error } from '@pnotify/core';
 
 const refs = {
   cardContainer: document.querySelector('.js-card-container'),
-  searchInput: document.querySelector('.input'),
+  searchInput: document.querySelector('.js-input'),
   countrySearchResult: document.querySelector('.country-list'),
 };
 
 refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(event) {
-//   event.preventDefault();
-  //   const inputTarget = event.currentTarget;
-
   const searchQuery = event.target.value;
-
-  if (searchQuery) {
-    fetchCountry(searchQuery).then(renderCountryCard).catch(onFetchError);
-    // .finally(() => (searchQuery = ''));
-  }
-}
-
-function renderCountryCard(resolvedCountries) {
-  let markup = '';
-
-  if (resolvedCountries.length > 10) {
-    error({
-      text: 'необходимо сделать запрос более специфичным',
-    });
-
-    if (resolvedCountries.length === 1) {
-      markup = countryTmpl(resolvedCountries);
-    }
-
-    if (resolvedCountries.length > 1 && resolvedCountries.length <= 10) {
-      markup = countryListTml(...resolvedCountries);
-    }
-  }
-  refs.countrySearchResult.innerHTML = markup;
+  fetchCountry(searchQuery).then(renderCountry).catch(onFetchError);
 }
 
 function fetchCountry(countryName) {
@@ -50,8 +24,8 @@ function fetchCountry(countryName) {
   });
 }
 
-function renderCountryCard(country) {
-  const markup = countryTmpl(country);
+function renderCountry(countryToRender) {
+  const markup = countryTmpl(countryToRender);
   refs.cardContainer.innerHTML = markup;
 }
 

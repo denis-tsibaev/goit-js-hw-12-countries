c2809685546a7825c599bb92f49baf4e9a0979a9
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-07-01 18:59:42 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './sass/main.scss';
 import countryTmpl from './templates/countryTmpl.hbs';
-import countryListTml from './templates/countryTmpl.hbs';
+import countryListTmpl from './templates/countryListTmpl.hbs';
 import debounce from 'lodash.debounce';
 import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
   cardContainer: document.querySelector('.js-card-container'),
   searchInput: document.querySelector('.js-input'),
-  countrySearchResult: document.querySelector('.country-list'),
 };
 
 refs.searchInput.addEventListener('input', debounce(onSearch, 500));
@@ -15,7 +15,6 @@ refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 function onSearch(event) {
   const searchQuery = event.target.value;
   fetchCountry(searchQuery).then(renderCountry).catch(onFetchError);
-  console.log(fetchCountry(searchQuery).then(renderCountry).catch(onFetchError));
 }
 
 function fetchCountry(countryName) {
@@ -26,11 +25,26 @@ function fetchCountry(countryName) {
 }
 
 function renderCountry(countryToRender) {
-  const markup = countryTmpl(countryToRender);
+  let markup = '';
+
+  if (countryToRender.length > 10) {
+    error({
+      title: 'ATTENTION!',
+      text: 'необходимо сделать запрос более специфичным',
+    });
+  }
+
+  if (countryToRender.length === 1) {
+    markup = countryTmpl(...countryToRender);
+  }
+
+  if (countryToRender.length > 1 && countryToRender.length <= 10) {
+    markup = countryListTmpl(countryToRender);
+  }
+
   refs.cardContainer.innerHTML = markup;
-//   console.log(markup)
 }
 
 function onFetchError() {
-  alert('something wrong! maybe name?');
-}
+	alert('something wrong! maybe name?');
+  }
